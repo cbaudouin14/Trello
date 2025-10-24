e00169352b9821b249e587ab2a9f94969e6f3098
cbaudouin14 <christopherbaudouin@gmail.com>
2025-10-24 17:00:43 +0200

    add remove localStorage

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".add-task-btn");
+  const buttons = document.querySelector(".add-task-btn");
   const form_btn = document.querySelector(".form_btn")
   const close_form = document.querySelector(".close_popup_form")
 
@@ -21,72 +21,75 @@ document.addEventListener("DOMContentLoaded", () => {
   };
 
   // Création des tâches
-  buttons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const form = document.getElementById("task_form");
-      form.classList.add("open");
+  buttons.addEventListener("click", () => {
+    const form = document.getElementById("task_form");
+    form.classList.add("open");
+  });
+
+  close_form.addEventListener("click", () => {
+    const form = document.getElementById("task_form");
+    form.classList.remove("open");
+  });
+
+  form_btn.addEventListener("click", () => {
+    const form = document.getElementById("task_form");
+    const title = document.getElementById("form_title").value;
+    const description = document.getElementById("form_description").value;
+
+    if (!title || title.trim() === "") return;
+    if (description === null) return;
+
+    // Heure
+    const creationTime = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+    // Colonne
+    const targetId = buttons.dataset.target;
+    const container = document.getElementById(targetId);
+
+    const task = document.createElement("div");
+    task.classList.add("section_card");
+    task.setAttribute("draggable", "true");
+    task.id = "task-" + Date.now();
+
+    // Stockage
+    const taskStorage = {
+      id: task.id,
+      column: targetId,
+      title: title,
+      description: description,
+      time: creationTime
+    };
+    setTaskOnList(taskStorage, getTaskList());
+
+    task.innerHTML = `
+      <div class="card_color" style="background:${colors[targetId]}; height:40px; border-radius:10px 10px 0 0;"></div>
+      <div style="display:flex; justify-content:space-between; align-items:center; padding:0 10px;">
+        <h4 class="card_title" style="font-weight:500;">${title}</h4>
+        <button class="delete-task" title="Supprimer" style="border:none; background:none; cursor:pointer; font-size:16px;">🗑️</button>
+      </div>
+      <p style="margin: 5px 10px 2px 10px; font-size:14px;">${description}</p>
+      <p style="margin: 0 10px 10px 10px; font-size:12px; color:#555;">${creationTime}</p>
+    `;
+
+    // Ajout dans la colonne
+    container.appendChild(task);
+    makeDraggable(task);
+
+
+    const deleteBtn = task.querySelector(".delete-task");
+    deleteBtn.addEventListener("click", (e) => {
+      e.stopPropagation(); 
+      task.remove();
+        let taskList = getTaskList();
+        taskList = taskList.filter(t => t.id !== task.id);
+        localStorage.setItem('taskList', JSON.stringify(taskList));
+
     });
 
-    close_form.addEventListener("click", () => {
-      const form = document.getElementById("task_form");
-      form.classList.remove("open");
-    });
-
-    form_btn.addEventListener("click", () => {
-      const form = document.getElementById("task_form");
-      const title = document.getElementById("form_title").value;
-      const description = document.getElementById("form_description").value;
-
-      if (!title || title.trim() === "") return;
-      if (description === null) return;
-
-      // Heure
-      const creationTime = new Date().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
-
-      // Colonne
-      const targetId = button.dataset.target;
-      const container = document.getElementById(targetId);
-
-      const task = document.createElement("div");
-      task.classList.add("section_card");
-      task.setAttribute("draggable", "true");
-      task.id = "task-" + Date.now();
-
-      // Stockage
-      const taskStorage = {
-        id: task.id,
-        column: targetId,
-        title: title,
-        description: description,
-        time: creationTime
-      };
-      setTaskOnList(taskStorage, getTaskList());
-
-      task.innerHTML = `
-        <div class="card_color" style="background:${colors[targetId]}; height:40px; border-radius:10px 10px 0 0;"></div>
-        <div style="display:flex; justify-content:space-between; align-items:center; padding:0 10px;">
-          <h4 class="card_title" style="font-weight:500;">${title}</h4>
-          <button class="delete-task" title="Supprimer" style="border:none; background:none; cursor:pointer; font-size:16px;">🗑️</button>
-        </div>
-        <p style="margin: 5px 10px 2px 10px; font-size:14px;">${description}</p>
-        <p style="margin: 0 10px 10px 10px; font-size:12px; color:#555;">${creationTime}</p>
-      `;
-
-      // Ajout dans la colonne
-      container.appendChild(task);
-      makeDraggable(task);
-
-
-      const deleteBtn = task.querySelector(".delete-task");
-      deleteBtn.addEventListener("click", (e) => {
-        e.stopPropagation(); 
-        task.remove();
-      });
-
-      form.classList.remove("open");
-    });
+    form.classList.remove("open");
   });
 });
+
 
 function setTaskOnList(task, taskList) {
   taskList.push(task);
