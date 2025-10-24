47805a1366d223db6a48d3a5d49440304837b6eb
Heldeb <helene.deblock@gmail.com>
2025-10-24 13:40:37 +0200

    drag.js
    permettre le drag and drop des cartes - mais il faut encore que j'optimise pour que la carte sélectionnée vienne seule

---
+++ drag.js
@@ -0,0 +1,39 @@
+const draggables = document.querySelectorAll(".section_list_card");
+const droppables = document.querySelectorAll(".trello_section");
+
+draggables.forEach((section_list_card) => {
+    section_list_card.addEventListener("dragstart", () => {
+        section_list_card.classList.add("is-dragging");
+    });
+    section_list_card.addEventListener("dragend", () => {
+        section_list_card.classList.remove("is-dragging");
+    });
+});
+
+droppables.forEach((zone) => {
+    zone.addEventListener("dragover", (e) => {
+        e.preventDefault();
+        const bottomTask = insertAboveTask(zone, e.clientY);
+        const curTask = document.querySelector(".is-dragging");
+        if (!bottomTask) {
+            zone.appendChild(curTask);
+        } else {
+            zone.insertBefore(curTask, bottomTask);
+        }
+    });
+});
+
+const insertAboveTask = (zone, mouseY) => {
+    const els = zone.querySelectorAll(".section_list_card:not(.is-dragging)");
+    let closestTask = null;
+    let closestOffset = Number.NEGATIVE_INFINITY;
+    els.forEach((section_list_card) => {
+        const { top } = section_list_card.getBoundingClientRect();
+        const offset = mouseY - top;
+        if (offset < 0 && offset > closestOffset) {
+            closestOffset = offset;
+            closestTask = section_list_card;
+        }
+    });
+    return closestTask;
+};
